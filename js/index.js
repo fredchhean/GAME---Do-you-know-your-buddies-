b5ba5209cdc3e03452002784395273570cc3ebcb
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-01 16:00:33 +0200

    initialisation of files

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// const myQuestions = create a constructor function so you can replicate the model 
+
+console.log("AZ");
+
+function showResults(){
+
+}
+
+function changePictures(){
+
+}
+
+function builQuiz(){
+
+}
+
+function printButton(){
+
+}
