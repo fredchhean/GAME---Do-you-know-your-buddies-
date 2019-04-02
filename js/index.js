1b3298d7ade82d96fca1637165889d2c310601ac
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-02 14:27:41 +0200

    array created

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let tableauAnswer = [
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My biggest talent is","floss challenging","./Images/arnaud.png","./Images/susan.png","./Images/fred.png","./Images/susan.png"},
+    {"If I could eat only 3 foods for the rest of life, it would be","Ramen, Kebab, Pineapple","Ramen, Kebab, Pineapple","./Images/yang.png","./Images/miguel.png","./Images/yang.png"},
+    {"MMy biggest talent is","./Images/alex.png","./Images/yagmur.png","./Images/miguel.png","./Images/miguel.png"},
     {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
     {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
     {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
