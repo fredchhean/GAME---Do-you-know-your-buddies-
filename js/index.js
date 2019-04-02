9fc5c86457a60cee55ce831a5f5358b3198e2bfc
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-02 10:44:13 +0200

    array creation

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-// const myQuestions = create a constructor function so you can replicate the model 
-class fullQst{
-    contructor(qst,asw1,asw2,asw3,goodAsw){
-        this.qst = qst;
-        this.asw1 = asw1;
-        this.asw2 = asw2;
-        this.asw3 = asw3
-        this.goodAsw = goodAsw;
-    }
-}
+let tableauAnswer = [
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},  
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},      
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+];
 
-var q1 = new fullQst("What is your guilty pleasure song","asw1","asw2","asw3","goodAsw")
-var q2 = new fullQst("What is your guilty pleasure song","asw1","asw2","asw3","goodAsw")
-
-let array = [q1,q2];
-console.log(array);
-console.log(array[0][1]);
 //voir comment sont stockés les var q1 et q2// FAUT LES PUT DANS DES ARRAYS
 
 console.log(q1);
