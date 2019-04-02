01af624523b549009dcd5b3a21cb2e1c5b2833c3
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-02 14:53:32 +0200

    variable declarations

--- js/index.js
+++ js/index.js
@@ -20,6 +20,16 @@ let tableauAnswer = [
     {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
 ];
 
+
+///////Déclaration des variables à utiliser answer photo
+var answer1 = document.getElementById('answer1');
+var answer2 = document.getElementById('answer2');
+var answer3 = document.getElementById('answer3');
+var score = document.getElementById('score');
+
+///////Déclaration des variables à utiliser photo
+
+
 //voir comment sont stockés les var q1 et q2// FAUT LES PUT DANS DES ARRAYS
 
 console.log(q1);
