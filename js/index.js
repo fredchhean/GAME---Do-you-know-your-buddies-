904d343d022093e84e97efb66535cd3296442633
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-02 17:53:18 +0200

    before testing button

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-let tableauAnswer = [
-    {"My biggest talent is","floss challenging","./Images/arnaud.png","./Images/susan.png","./Images/fred.png","./Images/susan.png"},
-    {"If I could eat only 3 foods for the rest of life, it would be","Ramen, Kebab, Pineapple","Ramen, Kebab, Pineapple","./Images/yang.png","./Images/miguel.png","./Images/yang.png"},
-    {"MMy biggest talent is","./Images/alex.png","./Images/yagmur.png","./Images/miguel.png","./Images/miguel.png"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},  
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},      
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
-    {"My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"},    
+let tableau = [
+    ["My biggest talent is ","floss challenging","./Images/arnaud.png","./Images/susan.png","./Images/fred.png","./Images/susan.png"],
+    ["If I could eat only 3 foods for the rest of life, it would be ","Ramen, Kebab, Pineapple","Ramen, Kebab, Pineapple","./Images/yang.png","./Images/miguel.png","./Images/yang.png"],
+    ["MMy biggest talent is ","Being humble, I'm so humble that people should call me the humble god","./Images/alex.png","./Images/yagmur.png","./Images/miguel.png","./Images/miguel.png"],
+    ["My biggest talent is ","turning pop songs into country songs","./Images/fred.png","./Images/arnaud.png","./Images/alex.png","./Images/alex.png"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],  
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],      
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],    
+    ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"]    
 ];
 
 
@@ -26,8 +26,22 @@ var answer1 = document.getElementById('answer1');
 var answer2 = document.getElementById('answer2');
 var answer3 = document.getElementById('answer3');
 var score = document.getElementById('score');
+var goodAsw = tableau[i][5];
 
-///////Déclaration des variables à utiliser photo
+////
+function showQuiz{
+    tableau.forEach(function(question,quote,asw1,asw2,asw3,goodAsw){
+        var output = [xxxxxxxxx];
+        for(output === goodAsw);
+
+    
+
+    })
+}
+///
+
+var btn = document.querySelector('input');
+
 
 
 //voir comment sont stockés les var q1 et q2// FAUT LES PUT DANS DES ARRAYS
