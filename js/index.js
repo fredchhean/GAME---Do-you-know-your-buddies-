ee5ff34ac24ccb102a0d702158c4ec24f8aa70a9
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-03 15:51:50 +0200

    refactoring with Abi

--- js/index.js
+++ js/index.js
@@ -3,25 +3,67 @@
 
 let tableau = [
     ["My biggest talent is ","floss challenging","./Images/arnaud.png","./Images/susan.png","./Images/susan.png","./Images/susan.png"],
-    ["If I could eat only 3 foods for the rest of life, it would be ","Ramen, Kebab, Pineapple","/Images/arnaud.png","/Images/arnaud.png","/Images/arnaud.png","./Images/susan.png"],
-    // ["MMy biggest talent is ","Being humble, I'm so humble that people should call me the humble god","./Images/alex.png","./Images/yagmur.png","./Images/miguel.png","./Images/miguel.png"],
+    ["If I could eat only 3 foods for the rest of life, it would be ","Ramen, Kebab, Pineapple","/Images/arnaud.png","/Images/susan.png","/Images/arnaud.png","./Images/susan.png"],
+    ["My biggest talent is ","Being humble, I'm so humble that people should call me the humble god","./Images/susan.png","/Images/arnaud.png","/Images/arnaud.png","/Images/arnaud.png"],
     // ["My biggest talent is ","turning pop songs into country songs","./Images/fred.png","./Images/arnaud.png","./Images/alex.png","./Images/alex.png"],
     // ["My guilty pleasure song is XXX","asw1","asw2","asw3","asw3"],  
 ];
 
 // ///Set the first page
+//OOOOOOOOOOOOOKKKK only thing is to define i and make the loop
+function printImage(i){
+    let image1 = document.getElementById("answer1");
+    image1.src = tableau[0][2];
+    let image2 = document.getElementById("answer2");
+    image2.src = tableau[0][3];
+    let image3 = document.getElementById("answer3");
+    image3.src = tableau[0][4];
+    console.log(tableau[0][2])
+    console.log("dddddddd");
+    
+}
+
+function printTheQuote(){
+    let element = document.getElementById("quote");
+    console.log(element.innerHTML);
+    console.log(tableau[0][0]);
+     element.innerHTML = tableau[0][0] + tableau[0][1]
+}
 
 
 
-function setStep(){
-    for (var i = 0; i < tableau.length; i++){
+function setStep(i){
     var nextButton = document.getElementsByClassName("row");
     nextButton[0].addEventListener("click", printImage(i));
-    // nextButton[0].addEventListener("click", printTheQuote(i));
-
-}
+     nextButton[0].addEventListener("click", printTheQuote(i));
 }
 setStep();
+
+var response = document.querySelectorAll(".answer");
+
+var selectedResponse = response.forEach(el => {
+
+  return   el.addEventListener("click", function () {
+
+
+    //check to see if the answer is correct
+       return console.log("youhou", el.src)
+
+
+
+       //set time out, increase array index by 1 
+       //check to see if the array is empty (array.length)
+
+
+    })
+})
+
+
+
+
+
+console.log(selectedResponse);
+
 // var nextButton = document.getElementsByClassName("row");
 // nextButton[0].addEventListener("click", printImage);
 // nextButton[0].addEventListener("click", printTheQuote);
@@ -46,38 +88,18 @@ setStep();
 
 
 ///Get the button selected by the player
-var gameButton = document.querySelectorAll(".game-btn");
-for(let i =0; i<gameButton.length;i++){
-    gameButton[i].onclick = getId
-}
-// gameButton.onclick = getId();
-function getId() {
-    console.log(this.childNodes[0].id)
-    var userAsw = this.childNodes[0].id
-}
+// var gameButton = document.querySelectorAll(".game-btn");
+// for(let i =0; i<gameButton.length;i++){
+//     gameButton[i].onclick = getId
+// }
+// // gameButton.onclick = getId();
+// function getId() {
+//     console.log(this.childNodes[0].id)
+//     var userAsw = this.childNodes[0].id
+// }
 
 ///OOOOOOOOOOK only thing is to define i and make the loop
-function printTheQuote(i){
 
-    // console.log(tableau[i][0]);
-    
-    let element = document.getElementById("quote");
-    console.log(element.innerHTML);
-    console.log(tableau[0]);
-    element.innerHTML = tableau[i][0] + tableau [i][1];
-}
-
-//OOOOOOOOOOOOOKKKK only thing is to define i and make the loop
-function printImage(i){
-    console.log(tableau[i][2]);
-
-    let image1 = document.getElementById("answer1");
-    image1.src = tableau[i][2];
-    let image2 = document.getElementById("answer2");
-    image2.src = tableau[i][2];
-    let image3 = document.getElementById("answer3");
-    image3.src = tableau[i][2];
-}
 
 
 // console.log(tableau[2][2]);
